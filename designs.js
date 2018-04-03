bc10395e0fa08a4f2fc982217847c391a1cb09a9
Vibudh Mishra <vibudhm@gmail.com>
2018-04-03 11:22:27 -0500

    added the JS function to make the single app work

--- designs.js
+++ designs.js
@@ -3,8 +3,42 @@
 
 // When size is submitted by the user, call makeGrid()
 
+// variables declarations
+
+const table = document.getElementById("pixelCanvas");
+const colorPicker = document.getElementById("colorPicker");
+
+let height, width;
+
+//the table should be made when clicked submit after the input values of height and width is defined
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(e){
+  e.preventDefault();
+  makeGrid();
+})
+
+
 function makeGrid() {
+  //clears the existing table rows
+  table.innerHTML = '';
+  //store the user input value for width and height of the table
+  height = document.getElementById('inputHeight').value;
+  width = document.getElementById('inputWeight').value;
 
-// Your code goes here!
+  for (let i=0; i < height; i++) {
+    let row = table.insertRow(i);
+    for (let j=0; j < width; j++) {
+      let cell = row.insertCell(j);
 
+      // adding event listener to add the background color to the insertCell
+      cell.addEventListener("click", function(event){
+        cell.style.backgroundColor = colorPicker.value;
+      });
+
+      //remove the color from the cell background colorPicker
+      cell.addEventListener("dblclick", function(event){
+        cell.style.backgroundColor = "";
+      });
+    }
+  }
 }
